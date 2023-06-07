c70c2e0d7d9815c3517806a04ed580759001afae
aherrer2 <aherrer2@AHERRERA-5050>
2023-06-07 11:06:38 -0500

    Add project files.

---
+++ Exercise 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello and welcome to the program.  What year were you born?  This is YYYY/MM/DD format.");
+            string yearOfBirth = Console.ReadLine();
+            Console.WriteLine("Please type your month of birth here.");
+            string monthOfBirth = Console.ReadLine();
+            Console.WriteLine("Please type the day of the month you were born.");
+            string dayOfBirth = Console.ReadLine();
+        }
+    }
+}
---
+++ Exercise 1/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_1
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthdate { get; set; }
+    }
+}
