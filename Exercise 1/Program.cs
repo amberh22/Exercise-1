d47d134dc0c9c52b4be5130c8f902d1d149f60fa
aherrer2 <aherrer2@AHERRERA-5050>
2023-06-12 19:20:03 -0500

    Calculate DOB

--- Exercise 1/Program.cs
+++ Exercise 1/Program.cs
@@ -12,11 +12,31 @@ namespace Exercise_1
         {
             Console.WriteLine("Hello and welcome to the program.  What year were you born?  This is YYYY/MM/DD format.");
             string yearOfBirth = Console.ReadLine();
+            var yearOfBirthInt = ToInt(yearOfBirth);
+
             Console.WriteLine("Please type your month of birth here.");
             string monthOfBirth = Console.ReadLine();
+            var monthOfBirthInt = ToInt(monthOfBirth);
+
             Console.WriteLine("Please type the day of the month you were born.");
             string dayOfBirth = Console.ReadLine();
-            Console.WriteLine();
+            var dayOfBirthInt = ToInt(dayOfBirth);
+
+            //Combine all of the ints into one date of birth.
+            DateTime newdate = new DateTime(yearOfBirthInt, monthOfBirthInt, dayOfBirthInt);
+            Console.WriteLine(newdate.ToShortDateString);
+        }
+        public static int ToInt(string value)
+        {
+            var success = int.TryParse(value, out var result);
+            if (success)
+            {
+                return result;
+            }
+            Console.WriteLine($"Your value {value} is not a number.  Please enter your value again");
+             
+            Main(null);
+            return 0;
         }
     }
 }
